aca56897d0bff79fda29a379ecb24b67766932d5
aakash2323 <aakash.beyondexs27@gmail.com>
2024-09-30 11:35:19 +0530

    file

--- src/apps/pages/Empolyee/Editchechin.jsx
+++ src/apps/pages/Empolyee/Editchechin.jsx
@@ -77,8 +77,14 @@ const Editcheckin = () => {
     disable: data.WorkAtHome === "Y" ? false : true,
   };
 
-  const Fnsave = async (values) => {
-    let action = mode === "A" ? "insert" : "update";
+  const Fnsave = async (values,del) => {
+    // let action = mode === "A" ? "insert" : "update";
+    let action =
+    mode === "A" && !del
+      ? "insert"
+      : mode === "E" && del
+      ? "harddelete"
+      : "update";
     var isCheck = "N";
     if (values.disable == false) {
       isCheck = "Y";
@@ -526,9 +532,27 @@ const Editcheckin = () => {
                     >
                       Save
                     </Button>
+                  )} {YearFlag == "true" ? (
+                    <Button
+                      color="error"
+                      variant="contained"
+                      onClick={() => {
+                        Fnsave(values,  "harddelete");
+                      }}
+                    >
+                      Delete
+                    </Button>
+                  ) : (
+                    <Button
+                      color="error"
+                      variant="contained"
+                      disabled={true}
+                    >
+                      Delete
+                    </Button>
                   )}
                   <Button
-                    color="error"
+                    color="warning"
                     variant="contained"
                     onClick={() => {
                       navigate("/Apps/TR123/Check In");
