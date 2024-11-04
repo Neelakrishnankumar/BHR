3db5d68e7e4040a1b92515843d29b8b98691b440
aakash2323 <aakash.beyondexs27@gmail.com>
2024-11-04 11:24:53 +0530

    file

--- src/apps/pages/HR/Editproject.jsx
+++ src/apps/pages/HR/Editproject.jsx
@@ -65,8 +65,14 @@ const Editproject = () => {
     disable: data.Disable === "Y" ? true : false,
   };
 
-  const Fnsave = async (values) => {
-    let action = mode === "A" ? "insert" : "update";
+  const Fnsave = async (values,del) => {
+    // let action = mode === "A" ? "insert" : "update";
+    let action =
+    mode === "A" && !del
+      ? "insert"
+      : mode === "E" && del
+      ? "harddelete"
+      : "update";
     var isCheck = "N";
     if (values.disable == true) {
       isCheck = "Y";
@@ -272,6 +278,24 @@ const Editproject = () => {
                       disabled={true}
                     >
                       Save
+                    </Button>
+                  )} {YearFlag == "true" ? (
+                    <Button
+                      color="error"
+                      variant="contained"
+                      onClick={() => {
+                        Fnsave(values,  "harddelete");
+                      }}
+                    >
+                      Delete
+                    </Button>
+                  ) :  (
+                    <Button
+                      color="error"
+                      variant="contained"
+                      disabled={true}
+                    >
+                      Delete
                     </Button>
                   )}
                   <Button
