9ce5c3178d625da73f8719917e5e3a4a97705d3e
radhicse199 <rathika1999cse@gmail.com>
2026-07-22 18:41:31 +0530

    file

--- src/apps/pages/Empolyee/EditTermfeestruct.jsx
+++ src/apps/pages/Empolyee/EditTermfeestruct.jsx
@@ -129,6 +129,8 @@ const EditTermfeestructure = () => {
     const { toggleSidebar, broken, rtl } = useProSidebar();
     const location = useLocation();
     const rowData = location.state || {};
+    console.log(rowData, "--find rowData");
+    
     const [subjectid, setSubjectid] = useState(null);
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
@@ -930,7 +932,7 @@ const handleSaveButtonClick = async (values, validateForm, setTouched) => {
                                             AccessID: "2054",
                                             ScreenName: "Standard",
                                             VerticalLicense: Subscriptionlastthree,
-                                            Filter: `parentID='${compID}'`,
+                                            Filter: `parentID='${compID}' AND AcademicYearID='${rowData.AcademicYearID}'`,
                                             Any: "",
                                         },
                                     })}`}
